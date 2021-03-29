7eb94dffa4f007a98cf28ef55850aac36c4a496e
Aleksei <axe455990@mail.ru>
2021-03-29 16:33:49 +0700

    My fifth commit

--- review_and_comments/models.py
+++ review_and_comments/models.py
@@ -1,12 +1,12 @@
-from django.contrib.auth import get_user_model
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-User = get_user_model()
+from content.models import Title
+from user_management.models import User
 
 
 class Review(models.Model):
-    title = models.ForeignKey(Title, related_name='reviews')
+    title = models.ForeignKey(Title, on_delete=models.CASCADE, related_name='reviews')
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
