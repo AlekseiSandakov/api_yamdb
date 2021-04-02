693e96c672534801ab7ac82c2a58c7f805f89311
Сергей <sergey_sorokin.1580@mail.ru>
2021-04-03 04:18:28 +0600

    review 1

--- content/views.py
+++ content/views.py
@@ -1,11 +1,10 @@
 from django.db.models import Avg
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, mixins, viewsets
-from rest_framework.permissions import AllowAny
 
 from .filters import TitleFilter
 from .models import Category, Genre, Title
-from .permissions import IsAdmin
+from .permissions import IsAdminOrReadOnly
 from .serializers import CategorySerializer, GenreSerializer, TitleSerializer
 
 
@@ -20,7 +19,7 @@ class TitleViewSet(viewsets.ModelViewSet):
     queryset = Title.objects.annotate(
         rating=Avg('reviews__score')).order_by('name')
     serializer_class = TitleSerializer
-    permission_classes = [AllowAny, IsAdmin]
+    permission_classes = [IsAdminOrReadOnly]
     filter_backends = [DjangoFilterBackend]
     filterset_class = TitleFilter
     filterset_fields = ['genre', ]
@@ -29,7 +28,7 @@ class TitleViewSet(viewsets.ModelViewSet):
 class GenreViewSet(Set):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
-    permission_classes = [AllowAny, IsAdmin]
+    permission_classes = [IsAdminOrReadOnly]
     filter_backends = [filters.SearchFilter]
     search_fields = ('name', 'slug')
     lookup_field = 'slug'
@@ -38,7 +37,7 @@ class GenreViewSet(Set):
 class CategoryViewSet(Set):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    permission_classes = [AllowAny, IsAdmin]
+    permission_classes = [IsAdminOrReadOnly]
     filter_backends = [filters.SearchFilter]
     search_fields = ('name', 'slug')
     lookup_field = 'slug'
